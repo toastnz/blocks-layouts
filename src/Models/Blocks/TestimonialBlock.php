bffcb2b4e713cd097a44752fdf2f92ed2e617ea4
Priscilla Yap <priscilla@toast.co.nz>
2022-08-26 11:42:05 +1200

    added testimonial block

---
+++ src/Models/Blocks/TestimonialBlock.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Toast\Blocks;
+
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldDeleteAction;
+use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
+use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
+use Toast\Blocks\Items\TestimonialBlockItem;
+
+class TestimonialBlock extends Block
+{
+    private static $table_name = 'Blocks_TestimonialBlock';
+    
+    private static $singular_name = 'Testimonial';
+    
+    private static $plural_name = 'Testimonial';
+
+    private static $db = [
+    ];
+
+    private static $has_many = [
+        'Items' => TestimonialBlockItem::class
+    ];
+
+    public function getCMSFields()
+    {
+        $this->beforeUpdateCMSFields(function ($fields) {
+            $fields->removeByName('Items');
+
+            if ($this->ID) {
+                $testimonialConfig = GridFieldConfig_RelationEditor::create(10);
+                $testimonialConfig->addComponent(GridFieldOrderableRows::create('SortOrder'))
+                    ->removeComponentsByType(GridFieldDeleteAction::class)
+                    ->addComponent(new GridFieldDeleteAction(false))
+                    ->removeComponentsByType('GridFieldAddExistingAutoCompleter');
+        
+                $testimonialBlockGridField = GridField::create(
+                    'Items',
+                    'Testimonial Items',
+                    $this->owner->Items(),
+                    $testimonialConfig
+                );    
+                $fields->addFieldToTab('Root.Main', $testimonialBlockGridField);
+            }
+        });
+
+        return parent::getCMSFields();
+    }
+
+}
---
+++ src/Models/Items/TestimonialBlockItem.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Toast\Blocks\Items;
+
+use Toast\Blocks\LinkBlock;
+use SilverStripe\Assets\File;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\TextField;
+use Sheadawson\Linkable\Models\Link;
+use SilverStripe\Forms\TextareaField;
+use Sheadawson\Linkable\Forms\LinkField;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use Toast\Blocks\TestimonialBlock;
+
+class TestimonialBlockItem extends BlockItem
+{
+    private static $table_name = 'Blocks_TestimonialBlockItem';
+
+    private static $db = [
+        'SortOrder' => 'Int',
+        'Name'      => 'Varchar(255)',
+        'Position'  => 'Varchar(255)',
+        'Summary'   => 'Text'
+    ];
+
+    private static $has_one = [
+        'Image'  => Image::class,
+        'Parent' => TestimonialBlock::class
+    ];
+
+    private static $summary_fields = [
+        'Name'      => 'Name',
+        'Position'  => 'Position',
+        'Summary'   => 'Summary'
+    ];
+
+    private static $searchable_fields = [
+        'Name',
+        'Position',
+        'Summary'
+    ];
+
+    private static $owns = [
+        'Icon',
+        'Image',
+    ];
+
+    public function getCMSFields()
+    {
+        $this->beforeUpdateCMSFields(function ($fields) {
+
+            $fields->addFieldsToTab('Root.Main', [
+                UploadField::create('Image', 'Thumbnail')
+                    ->setFolderName('Uploads/Blocks'),
+                TextField::create('Name', 'Name'),
+                TextField::create('Position', 'Position'),
+                TextareaField::create('Summary', 'Summary')
+            ]);
+
+        });
+
+        return parent::getCMSFields();
+    }
+}
