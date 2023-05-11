715402d8646a00d7891629c9e1602d87ef5b46a2
emmanuel-toast <emmanuel@toast.co.nz>
2023-05-11 12:38:41 +1200

    added BlogBlock and ChildrenBlock code from marmalade, into the module

--- src/Models/Blocks/BlogBlock.php
+++ src/Models/Blocks/BlogBlock.php
@@ -2,25 +2,89 @@
 
 namespace Toast\Blocks;
 
+use Sheadawson\Linkable\Forms\LinkField;
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
+use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
+use SilverStripe\Forms\LiteralField;
+use SilverStripe\Blog\Model\Blog;
+use SilverStripe\Blog\Model\BlogPost;
+use SilverStripe\Forms\GridField\GridFieldAddNewButton;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+use Heyday\ColorPalette\Fields\ColorPaletteField;
+use SilverStripe\SiteConfig\SiteConfig;
+use Toast\Helpers\Helper;
+
 class BlogBlock extends Block
 {
+    private static $table_name = 'Blocks_BlogBlock';
     private static $singular_name = 'Blog Block';
-
     private static $plural_name = 'Blog Blocks';
 
-    private static $description = 'Blog Block';
+    private static $db = [
+        'Content' => 'HTMLText',
+        'Columns'  => 'Enum("2,3,4", "2")'
+    ];
+    
+    private static $has_one = [
+        'Blog' => Blog::class
+    ];
 
-    private static $table_name = 'BlogBlock';
+    private static $many_many = [
+        'BlogPosts' => BlogPost::class
+    ];
 
-    private static  = [
+    private static $many_many_extraFields = [
+        'BlogPosts' => [
+            'SortOrder' => 'Int'
+        ]
     ];
 
     public function getCMSFields()
     {
-         = parent::getCMSFields();
+        $fields = parent::getCMSFields();
 
-        return ;
+        $config = GridFieldConfig_RelationEditor::create(4);
+        $config->removeComponentsByType(GridFieldAddNewButton::class)
+         ->addComponent(GridFieldOrderableRows::create('SortOrder'));
+         
+        $fields->removeByName(['BlogPosts']);
+        
+        if ($this->exists()) {
+
+            $siteConfig = SiteConfig::current_site_config();
+
+            $grid = GridField::create('BlogPosts', 'Blog Posts', $this->BlogPosts(), $config);
+
+            $fields->addFieldsToTab('Root.Main', [
+                DropdownField::create('Columns', 'Columns', $this->dbObject('Columns')->enumValues()),
+                DropdownField::create('BlogID', 'Blog', Blog::get()->map('ID', 'Title'))->setEmptyString('--Please select a blog--'),
+                LiteralField::create('Notice', '<div class="message notice">Latest blog posts will be displayed if no blog posts are linked, Blog will need to be selected.</div>'),
+                HTMLEditorField::create('Content', 'Content'),
+                $grid
+            ]);
+            
+        }else{
+            $fields->addFieldToTab('Root.Main', LiteralField::create('Notice', '<div class="message notice">Save this block and more options will become available.</div>'));
+        }
+
+        return $fields;
     }
+
+    public function getPosts($limit = 3)
+    {
+       
+        if (!$this->BlogPosts()->exists()){
+            if( $this->BlogID ){
+                return BlogPost::get()->filter(["ParentID" => $this->BlogID ])->limit($limit);
+            }
+        }
+        
+        return $this->BlogPosts()->limit($limit);
+    }
+ 
 }
 
 
--- src/Models/Blocks/ChildrenBlock.php
+++ src/Models/Blocks/ChildrenBlock.php
@@ -2,25 +2,85 @@
 
 namespace Toast\Blocks;
 
+use Sheadawson\Linkable\Forms\LinkField;
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
+use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
+use SilverStripe\Forms\LiteralField;
+use SilverStripe\Blog\Model\Blog;
+use SilverStripe\Blog\Model\BlogPost;
+use SilverStripe\Forms\GridField\GridFieldAddNewButton;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+use Heyday\ColorPalette\Fields\ColorPaletteField;
+use SilverStripe\SiteConfig\SiteConfig;
+use Toast\Helpers\Helper;
+
 class ChildrenBlock extends Block
 {
+    private static $table_name = 'Blocks_ChildrenBlock';
     private static $singular_name = 'Children Block';
-
     private static $plural_name = 'Children Blocks';
 
-    private static $description = 'Children Block';
+    private static $db = [
+        'Content' => 'HTMLText',
+        'Columns'  => 'Enum("2,3,4", "2")'
+    ];
 
-    private static $table_name = 'ChildrenBlock';
+    private static $many_many = [
+        'Children' => \Page::class
+    ];
 
-    private static  = [
+    private static $many_many_extraFields = [
+        'Children' => [
+            'SortOrder' => 'Int'
+        ]
     ];
 
     public function getCMSFields()
     {
-         = parent::getCMSFields();
+        $fields = parent::getCMSFields();
 
-        return ;
+        $config = GridFieldConfig_RelationEditor::create(4);
+        $config->removeComponentsByType(GridFieldAddNewButton::class)
+         ->addComponent(GridFieldOrderableRows::create('SortOrder'));
+         
+        $fields->removeByName(['Children']);
+        
+        if ($this->exists()) {
+            $grid = GridField::create('Children', 'Child Pages', $this->Children(), $config);
+
+            $fields->addFieldsToTab('Root.Main', [
+                DropdownField::create('Columns', 'Columns', $this->dbObject('Columns')->enumValues()),
+                HTMLEditorField::create('Content', 'Content'),
+                $grid
+            ]);
+
+            // if ($array = Helper::getThemeColoursArray()){
+            //     $fields->insertAfter('Columns',ColorPaletteField::create('BGColour', 'Background Colour',$array)
+            //         ->setDescription('Colours added from Site Settings'));
+            // }
+
+        }else{
+            $fields->addFieldToTab('Root.Main', LiteralField::create('Notice', '<div class="message notice">Save this block and more options will become available.</div>'));
+        }
+
+        return $fields;
     }
+
+    public function getChildPages()
+    {
+       
+        if (!$this->Children()->exists()){
+            if( $this->ParentID ){
+                return SiteTree::get()->filter(["ID" => $this->ParentID ]);
+            }
+        }
+        
+        return $this->Children();
+    }
+ 
 }
 
 
