dad8ee227ebc9b0b2a7ef970f79ea9b243feab21
Priscilla Yap <priscilla@toast.co.nz>
2023-06-29 11:09:18 +1000

    update resources dir

--- _config.php
+++ _config.php
@@ -2,5 +2,5 @@
 
 define('TOAST_BLOCKS_IMAGE_DIR', 'vendor/toastnz/' . basename(__DIR__) . '/client/images/layout-icons/default/');
 define('TOAST_BLOCKS_DIR', 'vendor/toastnz/' .   basename(__DIR__));
-define('TOAST_RESOURCES_DIR', 'resources/vendor/');
+define('TOAST_RESOURCES_DIR', RESOURCES_DIR . '/vendor');
 define('TOAST_BLOCKS_TEMPLATE_DIR','templates/Toast/Blocks/Default');
