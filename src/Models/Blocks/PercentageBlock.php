1fd327d063e5925792bab08a912ce307ed153ee3
emmanuel-toast <emmanuel@toast.co.nz>
2023-11-08 12:10:52 +1300

    removing limit on percentage block

--- src/Models/Blocks/PercentageBlock.php
+++ src/Models/Blocks/PercentageBlock.php
@@ -31,9 +31,6 @@ class PercentageBlock extends Block
         $this->beforeUpdateCMSFields(function ($fields) {
 
             $fields->removeByName('Items');
-            // set default to always have limit
-            $limit = true;
-            $limitCount = 4;
 
             if ($this->ID) {
                 $percentageConfig = GridFieldConfig_RecordEditor::create(10);
@@ -42,20 +39,13 @@ class PercentageBlock extends Block
                     ->removeComponentsByType(GridFieldDeleteAction::class)
                     ->addComponent(new GridFieldDeleteAction(false))
                     ->removeComponentsByType('GridFieldAddExistingAutoCompleter');
-                // allow to disable the limit and update limitCount
-                $this->extend('updateLimitItems', $limit, $limitCount);
-                // if limit is set to true, Limit to 4
-                if ($limit && $this->Items()->Count() >= $limitCount) {
-                    // remove the buttons if we don't want to allow more records to be added/created
-                    $percentageConfig->removeComponentsByType(GridFieldAddNewButton::class);
-                }
-        
+
                 $percentageBlockGridField = GridField::create(
                     'Items',
                     'Percentage Block Items',
                     $this->owner->Items(),
                     $percentageConfig
-                );    
+                );
                 $fields->addFieldToTab('Root.Main', $percentageBlockGridField);
             }
 
@@ -64,5 +54,5 @@ class PercentageBlock extends Block
         return parent::getCMSFields();
     }
 
-    
+
 }
