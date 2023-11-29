d7f1700abe14e6f0c4c5f0b90a440a27801d781e
emmanuel-toast <emmanuel@toast.co.nz>
2023-11-30 10:54:36 +1300

    new icon for text block and user form block

--- src/Models/Blocks/TextBlock.php
+++ src/Models/Blocks/TextBlock.php
@@ -3,26 +3,29 @@
 namespace Toast\Blocks;
 
 use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
 use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
 use SilverStripe\Forms\RequiredFields;
 
 class TextBlock extends Block
 {
     private static $table_name = 'Blocks_TextBlock';
-    
+
     private static $singular_name = 'Text';
-    
+
     private static $plural_name = 'Text';
 
     private static $db = [
+        'Heading' => 'Varchar(255)',
         'Content' => 'HTMLText'
     ];
 
     public function getCMSFields()
     {
         $this->beforeUpdateCMSFields(function ($fields) {
-        
+
             $fields->addFieldsToTab('Root.Main', [
+                TextField::create('Heading', 'Heading'),
                 HTMLEditorField::create('Content', 'Content')
             ]);
 
