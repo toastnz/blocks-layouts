c18f25a5e0faec8fdf51ebb50cc5cfe3e608ac09
Emmanuel Davies <emmanuel@toast.co.nz>
2024-04-15 13:27:31 +1200

    adding sort order to percentage block

--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -16,7 +16,7 @@ use SilverStripe\ORM\FieldType\DBHTMLText;
 use SilverStripe\Subsites\Model\Subsite;
 use SilverStripe\Subsites\State\SubsiteState;
 
-class Helper 
+class Helper
 {
     static function getAvailableBlocks()
     {
@@ -41,7 +41,7 @@ class Helper
     static function getLayoutIconSrc()
     {
         $availableBlocks = Config::inst()->get('Toast\Blocks\Extensions\PageExtension', 'layout_icon_src');
-      
+
         if (!file_exists(Director::baseFolder() . '/' . $availableBlocks)) {
             return ;
         }
@@ -49,7 +49,7 @@ class Helper
     }
 
     static function getAvailableBlocksLayouts($block, $src, $imgsrc, $default = true)
-    { 
+    {
         $optionset = [];
         // if this is optional directory
         // TODO: merge layouts with other specified folder in yml
@@ -61,37 +61,37 @@ class Helper
     }
 
 
-    static function getLayoutIcons($block,$src, $imgsrc, $default = true)
+    static function getLayoutIcons($block, $src, $imgsrc, $default = true)
     {
-       
+
         $currentTemplateName = strtolower($block->getBlockTemplateName());
         $optionset = [];
         $layouts = [];
         $icons = [];
         $basePath = BASE_PATH . '/' ;
-        
+
         $extensions = array('jpg', 'jpeg', 'png', 'gif', 'bmp', 'svg');
 
-        if (is_dir($src) && is_dir( $imgsrc)) {     
+        if (is_dir($src) && is_dir( $imgsrc)) {
             // get icons for layouts
             // if folder not exist in app, fall back to module default
             if ($directory = new DirectoryIterator($imgsrc)){
-              
+
                 $imgsrc_dir = basename($imgsrc);
                 $src_dir = basename($src);
                 foreach ($directory as $fileinfo){
-                   
+
                     if ($fileinfo->isFile()){
                         $imgfilename = $fileinfo->getFilename();
                         $extension = strtolower(pathinfo($imgfilename, PATHINFO_EXTENSION));
                         // Only add to our available icons if it's an extension we're after
-                        if (in_array($extension, $extensions)){					
+                        if (in_array($extension, $extensions)){
                             $imgfilename = pathinfo($imgfilename)['filename'];
                             $icons[$imgsrc.$fileinfo->getFilename()] = $imgfilename;
                         }
                     }
-                } 
-                
+                }
+
                 foreach (glob($src . "*.ss") as $filename) {
                     if ($name = pathinfo($filename)['filename']){
                             $name = strtolower($name);
@@ -99,13 +99,13 @@ class Helper
                     $className = 'Toast\Blocks\\' . $src_dir . '\\' . pathinfo($filename)['filename'];
                     // if this block is in the icon array value, add it to the optionset
                     if ($name == $currentTemplateName && in_array($name, array_values($icons))) {
-                        // get the index 
+                        // get the index
                         $index = array_search($name, array_values($icons));
                         // array of keys in the img src array
                         $filepath_keys   = array_keys( $icons );
-                        // assign img path 
+                        // assign img path
                         $matchedBlockImgSrcPath = $filepath_keys[$index];
-                    
+
                         // If the matchedBlockImgSrcPath is a .svg file
                         if (pathinfo($matchedBlockImgSrcPath, PATHINFO_EXTENSION) == 'svg') {
                             // Get the file contents from the matchedBlockImgSrcPath
@@ -114,14 +114,14 @@ class Helper
                         } else {
                             $thumbnail = '<img src="' . $matchedBlockImgSrcPath . '" />';
                         }
-                    
+
                         $html = '<div class="blockThumbnail">' . $thumbnail . '</div><strong class="title" title="Template file: ' . $filename . '">'.$imgsrc_dir.'</strong>';
                         $optionset[$className] = DBField::create_field(DBHTMLText::class, $html);
                     }
-                }   
+                }
             }
-             
-		}
+        }
+
         return $optionset;
     }
 
