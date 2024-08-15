2db0d805c36bd35e34a555987a649dcf28f48931
Emmanuel Davies <emmanuel@toast.co.nz>
2024-08-16 08:47:18 +1200

    adding in a dom observer to watch for the block's layouts html and convert svg imgs to html

---
+++ client/source/scripts/index.js
@@ -0,0 +1,57 @@
+/*------------------------------------------------------------------
+Import styles
+------------------------------------------------------------------*/
+
+import 'styles/index.scss';
+
+/*------------------------------------------------------------------
+Import modules
+------------------------------------------------------------------*/
+
+import DomObserverController from 'domobserverjs';
+
+/*------------------------------------------------------------------
+Dom Observer
+------------------------------------------------------------------*/
+
+const CMSObserver = new DomObserverController();
+
+/*------------------------------------------------------------------
+Document setup
+------------------------------------------------------------------*/
+
+// Observe the CMS for the toast-block-layouts fieldsets
+CMSObserver.observe('.toast-block-layouts', (fieldsets) => {
+  // Loop through the fieldsets
+  (async () => {
+    for (const fieldset of fieldsets) {
+      // Find all the images in the fieldset
+      const images = fieldset.querySelectorAll('img');
+
+      // Loop the images
+      for (const img of images) {
+        const imgSrc = img.src;
+
+        // If the img src does not include .svg, continue to the next image
+        if (!imgSrc.includes('.svg')) continue;
+
+        try {
+          // Fetch the svg, convert it to text, and replace the img with the svg
+          const response = await fetch(imgSrc);
+          const svg = await response.text();
+
+          // Create a new div
+          const div = document.createElement('div');
+
+          // Set the innerHTML of the div to the svg
+          div.innerHTML = svg;
+
+          // Replace the img with the div
+          img.parentNode.replaceChild(div.firstChild, img);
+        } catch (error) {
+          console.error(`Failed to fetch SVG: ${imgSrc}`, error);
+        }
+      }
+    }
+  })();
+});
