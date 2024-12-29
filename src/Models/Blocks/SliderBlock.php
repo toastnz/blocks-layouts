d62cdfbfa67a263f4d3448aeaaa3ef38ed37b823
emmanuel-toast <emmanuel@toast.co.nz>
2024-12-30 10:02:58 +1300

    simplifying the slider block

--- src/Models/Blocks/SliderBlock.php
+++ src/Models/Blocks/SliderBlock.php
@@ -3,13 +3,7 @@
 namespace Toast\Blocks;
 
 use Toast\Blocks\Block;
-use SilverStripe\Forms\TextField;
-use SilverStripe\Forms\LiteralField;
-use Toast\Blocks\Items\SliderBlockItem;
-use SilverStripe\Forms\GridField\GridField;
-use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
-use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
-use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
+use Bummzack\SortableFile\Forms\SortableUploadField;
 
 class SliderBlock extends Block
 {
@@ -19,27 +13,18 @@ class SliderBlock extends Block
 
     private static $plural_name = 'Media Slider';
 
-    private static $has_many = [
-        'Items'    => SliderBlockItem::class
+    private static $many_many = [
+        'Images' => Image::class,
     ];
 
     public function getCMSFields()
     {
-
         $this->beforeUpdateCMSFields(function ($fields) {
-            $fields->removeByName('Items');
-
-            if ($this->exists()) {
-                $config = GridFieldConfig_RecordEditor::create();
-                $config->addComponent(GridFieldOrderableRows::create('SortOrder'));
-                $grid = GridField::create('Items', 'Items', $this->Items(), $config);
-                $fields->addFieldsToTab('Root.Main', [
-                    $grid
-                ]);
-            }else{
-                $fields->addFieldToTab('Root.Main', LiteralField::create('Notice', '<div class="message notice">Save this block and more options will become available.</div>'));
-            }
+            $fields->addFieldsToTab('Root.Main', [
+                SortableUploadField::create('Images', 'Images')
+            ]);
         });
+
         return parent::getCMSFields();
     }
 
