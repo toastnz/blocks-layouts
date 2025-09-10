42b77510976d1b506f107179cb1e68485c56df14
emmanuel-toast <emmanuel@toast.co.nz>
2025-09-10 20:18:53 +1200

    fix for logo block cms fields

--- src/Models/Blocks/LogoBlock.php
+++ src/Models/Blocks/LogoBlock.php
@@ -3,7 +3,6 @@
 namespace Toast\Blocks;
 
 use Toast\Blocks\Block;
-use Toast\Blocks\LinkBlock;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\Forms\DropdownField;
 use Toast\Blocks\Items\LogoBlockItem;
@@ -34,45 +33,46 @@ class LogoBlock extends Block
 
     public function getCMSFields()
     {
-        $fields = parent::getCMSFields();
+        $this->beforeUpdateCMSFields(function ($fields) {
 
-        // Remove the default Items tab
-        $fields->removeByName('Items');
+            // Remove the default Items tab
+            $fields->removeByName('Items');
 
-        // Set up the options array
-        $options = [];
+            // Set up the options array
+            $options = [];
 
-        // Check to see if there are any columns available in the config
-        $columns = Config::inst()->get(LogoBlock::class, 'available_columns') ?? [2, 3, 4];
+            // Check to see if there are any columns available in the config
+            $columns = Config::inst()->get(LogoBlock::class, 'available_columns') ?? [2, 3, 4];
 
-        foreach ($columns as $column) {
-            $options[$column] = $column;
-        }
+            foreach ($columns as $column) {
+                $options[$column] = $column;
+            }
 
-        // Add the dropdown field to the main tab
-        if (count($options) > 0) {
-            $fields->addFieldsToTab('Root.Main', [
-                DropdownField::create('Columns', 'Columns', $options),
-            ]);
-        }
+            // Add the dropdown field to the main tab
+            if (count($options) > 0) {
+                $fields->addFieldsToTab('Root.Main', [
+                    DropdownField::create('Columns', 'Columns', $options),
+                ]);
+            }
 
-        if ($this->ID) {
-            $percentageConfig = GridFieldConfig_RecordEditor::create(10);
+            if ($this->ID) {
+                $percentageConfig = GridFieldConfig_RecordEditor::create(10);
 
-            $percentageConfig->addComponent(GridFieldOrderableRows::create('SortOrder'))
-                ->removeComponentsByType(GridFieldDeleteAction::class)
-                ->addComponent(new GridFieldDeleteAction(false))
-                ->removeComponentsByType('GridFieldAddExistingAutoCompleter');
+                $percentageConfig->addComponent(GridFieldOrderableRows::create('SortOrder'))
+                    ->removeComponentsByType(GridFieldDeleteAction::class)
+                    ->addComponent(new GridFieldDeleteAction(false))
+                    ->removeComponentsByType('GridFieldAddExistingAutoCompleter');
 
-            $percentageBlockGridField = GridField::create(
-                'Items',
-                'Logo Items',
-                $this->owner->Items(),
-                $percentageConfig
-            );
-            $fields->addFieldToTab('Root.Main', $percentageBlockGridField);
-        }
+                $percentageBlockGridField = GridField::create(
+                    'Items',
+                    'Logo Items',
+                    $this->owner->Items(),
+                    $percentageConfig
+                );
+                $fields->addFieldToTab('Root.Main', $percentageBlockGridField);
+            }
+        });
 
-        return $fields;
+        return parent::getCMSFields();
     }
 }
