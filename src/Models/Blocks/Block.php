0f646a70133afa3d0de70d7c61f77f808961b9fc
emmanuel-toast <emmanuel@toast.co.nz>
2026-01-06 08:11:15 +1300

    adding fallback to default layout if alternative layout is deleted

--- src/Models/Blocks/Block.php
+++ src/Models/Blocks/Block.php
@@ -117,7 +117,10 @@ class Block extends DataObject
 
         $this->extend('updateBlockTemplate', $template);
 
-        return $this->renderWith([$template, 'Toast\Blocks\Default\Block']);
+        // Default layout as fallback
+        $defaultTemplate = 'Toast\Blocks\Default\\' . $this->getBlockTemplateName();
+
+        return $this->renderWith([$template, $defaultTemplate, 'Toast\Blocks\Default\Block']);
     }
 
     public function getCMSFields()
