b8ed863b9a747eb8b8ddec94e1d78a3b555dbb7e
priscillay-toast <92568971+priscillay-toast@users.noreply.github.com>
2026-06-16 16:41:05 +1200

    update parentpage link for cms preview in MA and pages

--- src/Models/Blocks/Block.php
+++ src/Models/Blocks/Block.php
@@ -807,10 +807,8 @@ class Block extends DataObject
         }
 
         $pages = $this->getAllPages();
-        if(count($pages) === 0) {
-            return null;
-        }
-        return SiteTree::get()->byID($pages[0]);
+
+        return !empty($pages) ? $pages[0] : null;
     }
 
     protected function buildBlockLinkFromParent($parent, $action = null): string
@@ -1159,8 +1157,8 @@ class Block extends DataObject
             if (!$controller instanceof CMSPageEditController) {
                 try {
                     if ($data = $controller->data()) {
-                        if ($data->ID) {
-                            return SiteTree::get()->byID($data->ID);
+                        if ($data instanceof SiteTree && $data->ID) {
+                            return $data;
                         }
                     }
                 } catch (\Exception $e) {
